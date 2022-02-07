2f2bce6598011e4c475105627a93b42822c0df96
yonatonreid <yonatonreid@gmail.com>
2022-02-07 00:02:18 -0500

    upgrade to athena packages

--- src/Service/Listener/ErrorHandlerListener.php
+++ src/Service/Listener/ErrorHandlerListener.php
@@ -40,7 +40,6 @@ class ErrorHandlerListener extends \AthenaCore\Mvc\Service\Listener\AbstractServ
             $response -> getHeaders() -> addHeaderLine('Location', "{$base}/{$localeKey}/error");
             $response -> setStatusCode(MvcController::SERVER_ERROR);
         }
-        $response->sendHeaders();
         return $response;
     }
 }
