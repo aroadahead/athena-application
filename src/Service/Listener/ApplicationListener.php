78ec27eb2ca820616fdee1ebe9e78cfd476155be
yonatonreid <yonatonreid@gmail.com>
2022-02-16 18:09:52 -0500

    phpids

--- src/Service/Listener/ApplicationListener.php
+++ src/Service/Listener/ApplicationListener.php
@@ -1,10 +1,14 @@
 <?php
 declare(strict_types=1);
+
 namespace Application\Service\Listener;
 
 use AthenaCore\Mvc\Service\Listener\AbstractServiceListener;
+use IDS\Init;
+use IDS\Monitor;
 use Laminas\EventManager\EventManagerInterface;
 use Laminas\Mvc\MvcEvent;
+use function array_walk;
 
 class ApplicationListener extends AbstractServiceListener
 {
@@ -16,12 +20,48 @@ class ApplicationListener extends AbstractServiceListener
         $this -> attachAs($events, MvcEvent::EVENT_DISPATCH, [$this, 'onRoute'], $priority);
     }
 
-    public function onRoute(MvcEvent $e):void
+    /**
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Exception
+     */
+    public function onRoute(MvcEvent $e): void
     {
-        $this->markTriggered();
+        $this -> markTriggered();
 
-        if($e->getRequest()->isXmlHttpRequest()){
-            $e->getResult()->setTerminal(true);
+        if ($e -> getRequest() -> isXmlHttpRequest()) {
+            $e -> getResult() -> setTerminal(true);
+        }
+
+        if ($this -> container -> get('conf')
+            -> facade() -> getSecurityConfig('xss.phpids_scan_enabled')) {
+            $this -> scanXss($e);
+        }
+    }
+
+    private function scanXss(MvcEvent $e): void
+    {
+        $request = array(
+            'REQUEST' => $_REQUEST,
+            'GET' => $e -> getRequest() -> getQuery(),
+            'POST' => $e -> getRequest() -> getPost(),
+            'COOKIE' => $e -> getRequest() -> getCookie()
+        );
+        $facade = $this -> container -> get('conf') -> facade();
+        $configFile = $facade -> assets('phpids/Config.ini.php');
+        $init = Init ::init($configFile);
+        $overrides = $facade -> getSecurityConfig('xss.phpids') -> toArray();
+        foreach ($overrides as $key => $data) {
+            array_walk($data,
+                fn($e, $k) => $init -> setConfig(array($key => array($k => $e)), true));
+        }
+        $ids = new Monitor($init);
+        $result = $ids -> run($request);
+        if (!$result -> isEmpty()) {
+            $impact = $facade -> getSecurityConfig('xss.phpids_min_impact_level');
+            if ($impact >= $result -> getImpact()) {
+                die("XSS Attack Detected.");
+            }
         }
     }
 }
