09c83f5582ec073beb268d03abde145c7aa8d512
yonatonreid <yonatonreid@gmail.com>
2022-03-15 03:21:11 -0400

    exception manager

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -3,20 +3,27 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
+use AthenaBridge\Laminas\Authentication\AuthenticationService;
+use AthenaBridge\Laminas\Session\Container;
+use AthenaBridge\Laminas\Session\SessionManager;
+use AthenaBridge\Laminas\View\Model\FeedModel;
+use AthenaBridge\Laminas\View\Model\JsonModel;
+use AthenaBridge\Laminas\View\Model\ViewModel;
+use AthenaCore\Mvc\Application\Application\Core\ApplicationCore;
+use AthenaCore\Mvc\Application\Config\Facade\Facade;
+use AthenaCore\Mvc\Application\Laminas\Manager\Exception\ExceptionManager;
 use AthenaCore\Mvc\Controller\AbstractMvcController;
 use AthenaSodium\Entity\User;
 use http\Exception\InvalidArgumentException;
 use Interop\Container\ContainerInterface;
-use Laminas\Authentication\AuthenticationService;
 use Laminas\Filter\Word\CamelCaseToDash;
 use Laminas\Http\Response;
-use Laminas\View\Model\FeedModel;
-use Laminas\View\Model\JsonModel;
-use Laminas\View\Model\ViewModel;
+use Laminas\Uri\UriFactory;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use ReflectionClass;
 use ReflectionException;
+use function is_null;
 use function strlen;
 use function strpos;
 use function strtolower;
@@ -28,6 +35,21 @@ class ModuleController extends AbstractMvcController
      * Root namespace
      */
     protected string $rootNamespace;
+
+    /**
+     * Authentication Service
+     *
+     * @var AuthenticationService|null
+     */
+    private ?AuthenticationService $authenticationService = null;
+
+    private ?SessionManager $sessionManager = null;
+
+    private ?Facade $configFacade = null;
+
+    private ?ExceptionManager $exceptionManager = null;
+
+    private ?ApplicationCore $applicationCore = null;
 
     /**
      * Camelcase to dash filter
@@ -44,6 +66,21 @@ class ModuleController extends AbstractMvcController
         $namespace = (new ReflectionClass($this)) -> getNamespaceName();
         $namespace = substr($namespace, 0, strpos($namespace, '\\'));
         $this -> rootNamespace = strtolower(self ::$filter -> filter($namespace));
+        $this -> applicationCore = $this -> container -> get('core');
+        $this -> sessionManager = Container ::getDefaultManager();
+        $this -> exceptionManager = $this -> applicationCore -> getLaminasManager() -> getExceptionManager();
+        $this -> authenticationService = new AuthenticationService();
+        $this -> configFacade = $this -> container -> get('conf') -> facade();
+    }
+
+    public function core(): ApplicationCore
+    {
+        return $this -> applicationCore;
+    }
+
+    public function throwException(string $class, string $msg, array $args = []): void
+    {
+        $this -> exceptionManager -> throwException($class, $msg, $args);
     }
 
     /**
@@ -80,55 +117,119 @@ class ModuleController extends AbstractMvcController
         return new ViewModel([]);
     }
 
+    /**
+     * @param array $args
+     * @return ViewModel
+     */
     public function newViewModel(array $args = []): ViewModel
     {
         return new ViewModel($args);
     }
 
+    /**
+     * @param array $args
+     * @return JsonModel
+     */
     public function newJsonModel(array $args = []): JsonModel
     {
         return new JsonModel($args);
     }
 
+    /**
+     * @param array $args
+     * @return FeedModel
+     */
     public function newFeedModel(array $args = []): FeedModel
     {
         return new FeedModel($args);
     }
 
+    /**
+     * @return string
+     */
     public function redirectUrl(): string
     {
         $redirectUrl = $this -> params() -> fromQuery('r', '');
         if (strlen($redirectUrl) > 2048) {
-            throw new InvalidArgumentException("Redirect url too long.");
+            $this -> throwException(InvalidArgumentException::class, 'Redirect url too long.');
         }
         return $redirectUrl;
     }
 
+    /**
+     * @return AuthenticationService
+     */
+    public function authService(): AuthenticationService
+    {
+        return $this -> authenticationService;
+    }
+
+    /**
+     * @return SessionManager
+     */
+    public function sessionManager(): SessionManager
+    {
+        return $this -> sessionManager;
+    }
+
+    /**
+     * @return Facade
+     */
+    public function configFacade(): Facade
+    {
+        return $this -> configFacade;
+    }
+
+    /**
+     * @param string $redirectUrl
+     * @param bool $checkHost
+     * @return Response
+     */
+    public function verifyAndRedirectUrl(string $redirectUrl, bool $checkHost = true): Response
+    {
+        $uri = UriFactory ::factory($redirectUrl);
+        if (!$uri -> isValid()) {
+            $this -> throwException(InvalidArgumentException::class, 'invalid redirect uri: %s', [$redirectUrl]);
+        }
+        if ($checkHost) {
+            if (!is_null($uri -> getHost())) {
+                $this -> throwException(InvalidArgumentException::class, 'invalid redirect uri: %s', [$redirectUrl]);
+            }
+        }
+        return $this -> redirect() -> toUrl($redirectUrl);
+    }
+
+    /**
+     * @return User|null
+     */
     public function user(): User|null
     {
-        $authService = new AuthenticationService();
-        if (!$authService -> hasIdentity()) {
-            return null;
-        }
-        return $authService -> getIdentity();
+        return (($this -> authService() -> hasIdentity())
+            ? $this -> authService() -> getIdentity() : null);
     }
 
-    public function login():Response
+    /**
+     * @return Response
+     */
+    public function toLogin(): Response
     {
-        return $this->redirect()->toRoute('login');
+        return $this -> redirect() -> toRoute('login');
     }
 
-    public function notFound(): Response
+    /**
+     * @return Response
+     */
+    public function toNotFound(): Response
     {
         return $this -> redirect() -> toRoute('not-found');
     }
 
+    /**
+     * @return Response
+     */
     public function toDashboard(): Response
     {
-        $authService = new AuthenticationService();
-        if ($authService -> hasIdentity()) {
-            return $this -> redirect() -> toRoute('passport.dashboard');
-        }
-        return $this -> notFound();
+        return (($this -> authService() -> hasIdentity())
+            ? $this -> redirect() -> toRoute('passport.dashboard') : $this -> notFound());
     }
 }
